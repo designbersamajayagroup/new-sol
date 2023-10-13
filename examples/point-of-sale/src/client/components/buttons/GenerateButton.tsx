c4516b63d71a273ef879b23759e8ee29b75600b5
designbersamajayagroup <146061076+designbersamajayagroup@users.noreply.github.com>
2023-10-13 12:43:04 +0700

    disabled

--- examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
+++ examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
@@ -1,20 +1,18 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useRef } from 'react';
 import { usePayment } from '../../hooks/usePayment';
 import css from './GenerateButton.module.css';
 
 export const GenerateButton: FC = () => {
   const { amount, generate } = usePayment();
+  const generateButtonRef = useRef<HTMLButtonElement | null>(null);
 
   // Function to trigger button click based on URL parameter
   const autoClickGenerateButton = () => {
     const urlParams = new URLSearchParams(window.location.search);
     const ammountValue = urlParams.get("ammountValue");
 
-    if (ammountValue) {
-      const generateButton = document.getElementById("generateNow");
-      if (generateButton) {
-        generateButton.click();
-      }
+    if (ammountValue && generateButtonRef.current) {
+      generateButtonRef.current.click();
     }
   };
 
@@ -29,11 +27,12 @@ export const GenerateButton: FC = () => {
 
   return (
     <button
+      ref={generateButtonRef}
       className={css.root}
       type="button"
       onClick={handleClick}
       id="generateNow"
-      disabled={!amount || amount.isLessThanOrEqualTo(0)}
+      disabled={false}
     >
       Get the QR Code
     </button>
