4fd2f7b64d66ab756b1059ae083933cf2a205036
designbersamajayagroup <146061076+designbersamajayagroup@users.noreply.github.com>
2023-10-13 12:34:09 +0700

    button enabled

--- examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
+++ examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
@@ -1,9 +1,10 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { usePayment } from '../../hooks/usePayment';
 import css from './GenerateButton.module.css';
 
 export const GenerateButton: FC = () => {
   const { amount, generate } = usePayment();
+  const [isButtonEnabled, setIsButtonEnabled] = useState<boolean>(true);
 
   // Function to trigger button click based on URL parameter
   const autoClickGenerateButton = () => {
@@ -18,13 +19,27 @@ export const GenerateButton: FC = () => {
     }
   };
 
-  // Use useEffect to call the function when the component mounts
   useEffect(() => {
     autoClickGenerateButton();
   }, []);
 
+  // Check if the button should be enabled
+  useEffect(() => {
+    if (amount && amount.isGreaterThan(0.000000001)) {
+      setIsButtonEnabled(true);
+    } else {
+      setIsButtonEnabled(false);
+    }
+  }, [amount]);
+
   return (
-    <button className={css.root} type="button" onClick={generate} id="generateNow" disabled={!amount || amount.isLessThanOrEqualTo(0.000000001)}>
+    <button
+      className={css.root}
+      type="button"
+      onClick={generate}
+      id="generateNow"
+      disabled={!isButtonEnabled}
+    >
       Get the QR Code
     </button>
   );
