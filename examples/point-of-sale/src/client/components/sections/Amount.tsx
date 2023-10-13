3bfb6aa8aa5694b9ca048c29001146812908f02a
designbersamajayagroup <146061076+designbersamajayagroup@users.noreply.github.com>
2023-10-13 12:05:59 +0700

    Update Amount.tsx

--- examples/point-of-sale/src/client/components/sections/Amount.tsx
+++ examples/point-of-sale/src/client/components/sections/Amount.tsx
@@ -1,5 +1,5 @@
 import BigNumber from 'bignumber.js';
-import React, { FC, useMemo, useEffect } from 'react';
+import React, { FC, useMemo } from 'react';
 import { useConfig } from '../../hooks/useConfig';
 import { NON_BREAKING_SPACE } from '../../utils/constants';
 
@@ -14,30 +14,12 @@ export const Amount: FC<AmountProps> = ({ amount, showZero }) => {
     const value = useMemo(() => {
         if (!amount) return NON_BREAKING_SPACE;
         if (amount.isGreaterThan(0)) {
-            const decimals = amount.decimalPlaces() ?? 0;
+            const decimals = amount.decimalPlaces() ?? 0
             return amount.toFormat(decimals < minDecimals ? minDecimals : decimals);
         } else {
             return showZero ? '0' : NON_BREAKING_SPACE;
         }
     }, [amount, minDecimals, showZero]);
 
-    // Function to extract ammountValue from URL
-    const extractAmmountValueFromURL = () => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const ammountValue = urlParams.get("ammountValue");
-
-        if (ammountValue) {
-            // Update the value with the value from the URL
-            const ammountValueElement = document.getElementById("ammountValue");
-            if (ammountValueElement) {
-                ammountValueElement.textContent = ammountValue;
-            }
-        }
-    };
-
-    useEffect(() => {
-        extractAmmountValueFromURL();
-    }, []);
-
-    return <span id="ammountValue">{value}</span>;
+    return <span>{value}</span>;
 };
