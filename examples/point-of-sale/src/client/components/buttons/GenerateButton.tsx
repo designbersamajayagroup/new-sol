221007e1ce44ad1ed207d0a8dfeeeed6c8e0d687
designbersamajayagroup <146061076+designbersamajayagroup@users.noreply.github.com>
2023-10-13 12:21:42 +0700

    Update GenerateButton.tsx

--- examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
+++ examples/point-of-sale/src/client/components/buttons/GenerateButton.tsx
@@ -1,18 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { usePayment } from '../../hooks/usePayment';
 import css from './GenerateButton.module.css';
 
 export const GenerateButton: FC = () => {
-    const { amount, generate } = usePayment();
+  const { amount, generate } = usePayment();
 
-    return (
-        <button
-            className={css.root}
-            type="button"
-            onClick={generate}
-            id="generateNow"
-        >
-            Get the QR Code
-        </button> 
-    );
+  // Function to trigger button click based on URL parameter
+  const autoClickGenerateButton = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const ammountValue = urlParams.get("ammountValue");
+
+    if (ammountValue) {
+      const generateButton = document.getElementById("generateNow");
+      if (generateButton) {
+        generateButton.click();
+      }
+    }
+  };
+
+  // Use useEffect to call the function when the component mounts
+  useEffect(() => {
+    autoClickGenerateButton();
+  }, []);
+
+  return (
+    <button className={css.root} type="button" onClick={generate} id="generateNow">
+      Get the QR Code
+    </button>
+  );
 };
